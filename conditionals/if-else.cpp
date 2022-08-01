c4cdbc661f51caaf130fc00a482b1e4d51b7b4a5
Aditya Vijayvergiya <kiriotheo@gmail.com>
2022-08-01 19:30:35 +0530

    Create if-else.cpp

---
+++ conditionals/if-else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (void) {
+   int a;
+   cout << "Input Integer is : ";
+   cin >> a ;
+   
+
+   if (a < 0){
+      cout << "negative." << endl;
+   }
+   else {
+      cout << "positive." << endl;
+   }
+
+
+}
