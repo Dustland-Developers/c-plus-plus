2ecadb9c7d108471c9065110d07ed9942c4000c0
Aditya Vijayvergiya <kiriotheo@gmail.com>
2022-08-01 20:51:04 +0530

    Delete practice\ directory

--- practice\/size-of-datatype.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main (void){
-	int a ;
-	cout << "value of a just after intialization = " << a << endl;
-	cin >> a;
-	cout << "value of a just after cin statement = " << a << endl;
-	return 0;
-}
