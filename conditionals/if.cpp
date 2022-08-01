e9383efa74dfccfb917ea01828042f1ff8fa903e
Aditya Vijayvergiya <kiriotheo@gmail.com>
2022-08-01 19:39:59 +0530

    Create if.cpp

---
+++ conditionals/if.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+   int a;
+   cin >> a;
+   if (a < 0){
+      cout << "Input Integer is negative." << endl;
+   }
+   return 0;
+}
+
+/* Ouput : -4
+           Input Integer is negative. */
