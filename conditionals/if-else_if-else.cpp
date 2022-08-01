c1b741678182cd431df984bcb7583c9b9a0d60e8
Aditya Vijayvergiya <kiriotheo@gmail.com>
2022-08-01 19:12:11 +0530

    Create if-else_if-else.cpp

---
+++ conditionals/if-else_if-else.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main (){
+   int a;
+   cin >> a;
+   if (a > 0){
+      cout << "My name is aditya" << endl;
+   }
+
+   else if (a < 0){
+      cout << "My name is kiriotheo" << endl;
+   }
+   
+   else {
+      cout << "My Instagram Account: @adityavijay21" << endl;
+   }
+   return 0;
+}
+
+
+/* output : 0
+            My Instagram Account : @adityavijay21
+            
+  
